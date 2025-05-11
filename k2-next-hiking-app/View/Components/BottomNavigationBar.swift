2b7e345829b5db4b00386cac2c7ba9b9769e6a7f
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-05-11 12:03:17 +0300

    V1 BottomNavigationBar

---
+++ k2-next-hiking-app/View/Components/BottomNavigationBar.swift
@@ -0,0 +1,80 @@
+import SwiftUI
+
+struct BottomNavigationBar: View {
+    @State private var selectedView = "home-view"
+    
+    var body: some View {
+        HStack(spacing: 50) {
+            TabBarItem(icon: "house.fill",
+                       label: "Головна",
+                       isSelected: selectedView == "home-view",
+                       action: { selectedView = "home-view" }
+            )
+            
+            TabBarItem(icon: "point.bottomleft.forward.to.point.topright.scurvepath",
+                       label: "Маршрути",
+                       isSelected: selectedView == "route-view",
+                       isRotating: true,
+                       action: { selectedView = "route-view" }
+            )
+            
+            TabBarItem(icon: "person.fill",
+                       label: "Аккаунт",
+                       isSelected: selectedView == "account-view",
+                       action: { selectedView = "account-view" }
+            )
+        }
+    }
+}
+
+struct TabBarItem: View {
+    let icon: String
+    let label: String
+    let isSelected: Bool
+    let isRotating: Bool
+    let action: () -> Void
+    
+    @State private var isTapped = false
+    
+    init(icon: String, label: String, isSelected: Bool, isRotating: Bool = false, action: @escaping () -> Void) {
+        self.icon = icon
+        self.label = label
+        self.isSelected = isSelected
+        self.isRotating = isRotating
+        self.action = action
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: icon)
+                .resizable()
+                .frame(width: 25, height: 22)
+                .padding(.bottom, 0)
+                .scaleEffect(isTapped ? 1.3 : 1.0)
+                .rotation3DEffect(
+                    .degrees(isRotating && isSelected ? 180 : 0),
+                    axis: (x: 0, y: 1, z: 0)
+                )
+                .animation(isRotating ?
+                    .easeInOut(duration: 0.5) :
+                        .spring(response: 0.3, dampingFraction: 0.4), value: isTapped)
+            
+            Text(label)
+        }
+        .onTapGesture {
+            isTapped = true
+            action()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                isTapped = false
+            }
+        }
+        .foregroundColor(isSelected ?
+                         Color.instance.bottomBarActive :
+                            Color.instance.bottomBarNotActive)
+    }
+    
+}
+
+#Preview {
+    BottomNavigationBar()
+}
