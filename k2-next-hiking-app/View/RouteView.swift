92d65e3f5a98d3ff8efb5632430ccf006a38c18a
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-05-17 23:22:56 +0300

    tabview navaigation v1

---
+++ k2-next-hiking-app/View/RouteView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct RouteView: View {
+    var body: some View {
+        Text("route")
+    }
+}
+
+#Preview {
+    RouteView()
+}
--- k2-next-hiking-app/k2_next_hiking_appApp.swift
+++ k2-next-hiking-app/k2_next_hiking_appApp.swift
@@ -2,9 +2,39 @@ import SwiftUI
 
 @main
 struct k2_next_hiking_appApp: App {
+    
+    @State var selectedView: String = "home-view"
+    
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            TabView {
+                HomeView()
+                    .tabItem {
+                        TabBarItem(icon: "house.fill",
+                                   label: "Головна",
+                                   isSelected: selectedView == "home-view",
+                                   action: { selectedView = "home-view" }
+                        )
+                    }
+                RouteView()
+                    .tabItem {
+                        TabBarItem(icon: "point.bottomleft.forward.to.point.topright.scurvepath",
+                                   label: "Маршрути",
+                                   isSelected: selectedView == "route-view",
+                                   isRotating: true,
+                                   action: { selectedView = "route-view" }
+                        )
+                    }
+                UserAccountView()
+                    .tabItem {
+                        TabBarItem(icon: "person.fill",
+                                   label: "Аккаунт",
+                                   isSelected: selectedView == "account-view",
+                                   action: { selectedView = "account-view" }
+                        )
+                    }
+            }
+            .accentColor(Color.instance.greenButton)
         }
     }
 }
